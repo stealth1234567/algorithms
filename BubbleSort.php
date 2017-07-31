6dc023185eb87e264a3f092cb07bae0283b57c6c
Seun <stealth1234567@users.noreply.github.com>
2017-07-31 13:58:11 +0100

    Update BubbleSort.php
    cleaner implementation

--- BubbleSort.php
+++ BubbleSort.php
@@ -1,24 +1,22 @@
 <?php
   // Bubble Sort PHP Implementation
   function BubbleSort(&$array, $endIndex) {
-    if($endIndex > 0) {
-      for($i = 0; $i < $endIndex; $i++) {
-        if($array[$i] > $array[$i+1]) {
-          echo "SWAP {$array[$i]} with {$array[$i+1]}\n";
-          $temp = $array[$i];
-          $array[$i] = $array[$i+1];
-          $array[$i+1] = $temp;
-        }
-      }
-      $endIndex = $endIndex - 1;
-      BubbleSort($array, $endIndex);
+    if($endIndex === 0) {
+      return $array;
     }
-    return $array;
+    for($i = 0; $i < $endIndex; $i++) {
+      if($array[$i] > $array[$i+1]) {
+        echo "SWAP {$array[$i]} with {$array[$i+1]}\n";
+        $temp = $array[$i];
+        $array[$i] = $array[$i+1];
+        $array[$i+1] = $temp;
+      }
+    }
+    return BubbleSort($array, $endIndex - 1);
   }
-
+  
   // TEST
-  $array = [26,54,93,17];
-  $myArray = BubbleSort($array, 3);
-
+  $array = [ 26, 54, 93, 17, 77, 31, 44, 55, 20 ];
+  $myArray = BubbleSort($array, 8);
   print_r($myArray);
 ?>
