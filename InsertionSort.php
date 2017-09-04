d4da39c17ca5f86e0e98f077e3efca06d4868b0b
Seun <stealth1234567@users.noreply.github.com>
2017-09-04 21:46:45 +0100

    Update InsertionSort.php
    previous implementation was completely wrong.

--- InsertionSort.php
+++ InsertionSort.php
@@ -1,23 +1,15 @@
 <?php
-  function InsertionSort($array, $startIndex) {
-    if(count($array) === 1 || $startIndex >= count($array)) {
-      return $array;
-    }
-
-    for($i = $startIndex; $i > 0; $i--) {
-      if($array[$i] < $array[$i - 1]) {
-        $temp = $array[$i - 1];
-        $array[$i - 1] = $array[$i];
-        $array[$i] = $temp;
-      }
-    }
-    return InsertionSort($array, $startIndex + 1);
-  }
-
   $myArray = [2, 1, 10, 15, 5, 8, 8, 22, 0];
-  $myArray = InsertionSort($myArray, 1);
   
-  foreach($myArray as $item) {
-    echo $item . "\n";
+  function InsertionSort(&$array) {
+    for($i = 1; $i < count($array); $i++) {
+      $j = $i - 1;
+      $currentElement = $array[$i];
+      while($j >= 0 && ($array[$j] > $currentElement)) {
+        $array[$j+1] = $array[$j];
+        $j--;
+      }
+      $array[$j+1] = $currentElement;
+    }
   }
 ?>
