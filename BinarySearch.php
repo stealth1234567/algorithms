7b93c04395a175fec0f2f4ac06716e68f46d23f9
Seun <stealth1234567@users.noreply.github.com>
2017-09-11 15:12:17 +0100

    Update BinarySearch.php
    corrected error on line 10. moved middle calculation into if statement for efficiency and accuracy.

--- BinarySearch.php
+++ BinarySearch.php
@@ -1,13 +1,13 @@
 <?php
   function BinarySearch($arr, $key, $start, $end) {
-    $middle = floor(($start + $end) / 2); // need to use floor() in PHP
-    
     if($start <= $end) {
+      $middle = floor(($start + $end) / 2); // need to use floor() in PHP    
+        
       if($arr[$middle] == $key) {
         return $middle;
       }
       else if($arr[$middle] < $key) {
-        return BinarySearch($arr, $start, $middle+1, $end);
+        return BinarySearch($arr, $key, $middle+1, $end);
       }
       else {
         return BinarySearch($arr, $key, $start, $middle-1);
