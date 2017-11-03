af564c41c081c4abcc59594453776bdaa8ec429a
Seun <stealth1234567@users.noreply.github.com>
2017-11-03 09:36:51 +0000

    Create insertion_sort.java
    first implementation

---
+++ java/insertion_sort.java
@@ -0,0 +1,22 @@
+public class insertionSort {
+  public static void insertionSort(int[] arr) {
+    int size = arr.length;
+    for(int i = 1; i < size; i++) {
+      int currentElement = arr[i];
+      int j = i - 1;
+      while((j >= 0) && (arr[j] > currentElement)) {
+        arr[j+1] = arr[j];
+        j--;
+      }
+      arr[j+1] = currentElement;
+    }
+  }
+  
+  public static void main(String[] args) {
+    int[] array = {3,5,2,1,9,8,6,7,4};
+    insertionSort(array);
+    for (int i : array) {
+      System.out.println(i);
+    }
+  }
+}
